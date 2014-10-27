6162ea6b41d11df10d0e88d1d884751a5ca3ca71
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2014-10-27 12:32:27 +0000

    line delimited forms

--- src/Keboola/JsonparserApiBundle/Controller/ApiController.php
+++ src/Keboola/JsonparserApiBundle/Controller/ApiController.php
@@ -16,67 +16,79 @@ use Keboola\JsonparserApiBundle\JsonparserApi;
  */
 class ApiController extends Controller
 {
-	/**
-	 * @param Request $request
-	 */
-	public function convertAction(Request $request)
-	{
-		if ($request->getContentType() == "json") {
-		// for JSON in a request body
-			$data = $request->getContent();
-		} elseif (array() !== $request->files->all()) {
-		// JSON as a file attachment
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function postAction(Request $request)
+    {
+        $linedelimited = false;
+        if ($request->get("linedelimited")) {
+            $linedelimited = true;
+        }
+        $api = new JsonparserApi();
+
+        if ($request->get("json")) {
+            $data = $request->get("json");
+            if ($linedelimited) {
+                throw new HttpException(400, "Line delimited allowed only for file uploads.");
+            }
+            $result = $api->process($data);
+        } elseif ($request->getContentType() == "json") {
+      		// for JSON in a request body
+      		$data = $request->getContent();
+            if ($linedelimited) {
+                throw new HttpException(400, "Line delimited allowed only for file uploads.");
+            }
+            $result = $api->process($data);
+        } else {
+		    // JSON as a file attachment
 			if (count($request->files->all()) > 1) {
 				throw new HttpException(400, "Only one file at a time is supported using form-data");
 			}
-
+            if (array() == $request->files->all()) {
+                throw new HttpException(400, "No files have been attached in the form-data");
+            }
 			$file = array_values($request->files->all())[0];
 			if ($file->getClientSize() > 2*1024*1024) { // FIXME parameters.yml bsns
 				throw new HttpException(400, "Uploaded file exceeded 2MB");
 			}
+            $file = array_values($request->files->all())[0];
+            if ($linedelimited) {
+                $result = $api->processLineDelimited($file->openFile('r'));
+            } else {
+                $data = file_get_contents($file->getPathName());
+                $result = $api->process($data);
+            }
+        }
+        return $this->returnFileResponse($result);
 
-			$data = file_get_contents($file->getPathName());
-		} else {
-			throw new HttpException(400, "Request data must be in a JSON format!");
-		}
+    }
 
-		$api = new JsonparserApi();
-		$result = $api->process($data);
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function getAction(Request $request)
+   	{
+   		parse_str($request->getQueryString(), $query);
+   		$url = urldecode($query['url']);
+        $linedelimited = false;
+        if (isset($query['linedelimited'])) {
+            $linedelimited = true;
+        }
+   		$api = new JsonparserApi();
+   		$result = $api->processFromUrl($url, $linedelimited);
 
-		return $this->returnFileResponse($result);
-	}
+   		return $this->returnFileResponse($result);
+   	}
 
-	public function convertLineDelimitedAction(Request $request)
-	{
-		if (array() == $request->files->all()) {
-			throw new HttpException(400, "No files have been attached in the form-data");
-		}
-		if (count($request->files->all()) > 1) {
-			throw new HttpException(400, "Only one file at a time is supported using form-data");
-		}
-
-		$file = array_values($request->files->all())[0];
-
-		$api = new JsonparserApi();
-		$result = $api->processLineDelimited($file->openFile('r'));
-
-		return $this->returnFileResponse($result);
-	}
-
-	/**
-	 * @param Request $request
-	 */
-	public function getAction(Request $request, $ld = false)
-	{
-		parse_str($request->getQueryString(), $query);
-		$url = urldecode($query['url']);
-		$api = new JsonparserApi();
-		$result = $api->processFromUrl($url, true);
-
-		return $this->returnFileResponse($result);
-	}
-
-	protected function returnFileResponse($result)
+    /**
+     * @param $result
+     * @return Response
+     */
+    protected function returnFileResponse($result)
 	{
 		$response = new Response();
 
