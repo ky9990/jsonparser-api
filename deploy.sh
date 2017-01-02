72acf413dfc39dcf04edbe0e372bbf2a64b7c909
Vladimír Kriška <vlado@keboola.com>
2017-01-02 14:58:51 +0100

    Remove -e flag from docker login

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/json-parser quay.io/keboola/json-parser:$TRAVIS_TAG
 docker images
 docker push quay.io/keboola/json-parser:$TRAVIS_TAG
